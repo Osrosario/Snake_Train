27b64265df9bd7f4b1cc66012e426a5670d8c14f
Somnarng <99498786+Somnarng@users.noreply.github.com>
2023-05-10 16:57:25 -0400

    Re-Added Camera Controller, Fixed Object Poolers missing in Main Scenes

---
+++ Assets/Scripts/General/CameraController.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public Transform player;
+    public float cameraSpeed = 5f;
+    public float minX;
+    public float maxX;
+    public float minY;
+    public float maxY;
+    public float zPosition = -10f;
+
+    public Vector3 offset;
+
+
+
+    private void LateUpdate()
+    {
+        Vector3 targetPosition = player.position + offset;
+
+        // Clamp the camera position within the specified bounds.
+        targetPosition.x = Mathf.Clamp(targetPosition.x, minX, maxX);
+        targetPosition.y = Mathf.Clamp(targetPosition.y, minY, maxY);
+        targetPosition.z = zPosition;
+
+        transform.position = Vector3.Lerp(transform.position, targetPosition, Time.deltaTime * cameraSpeed);
+    }
+}
